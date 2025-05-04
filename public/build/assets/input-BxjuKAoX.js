79afe5d8f9d296fe50f38a5bbdcabdb21c47d034
Cletus Igwe <me@cletusigwe.com>
2025-05-04 22:47:05 +0100

    mvp improved 2.0

---
+++ public/build/assets/input-BxjuKAoX.js
@@ -0,0 +1,11 @@
+import{c as e,a as i}from"./AppLayout-DVArcdi2.js";import{r as n,j as a}from"./app-DbzSqnlF.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]],p=e("Search",c);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],b=e("X",d),f=n.forwardRef(({className:o,type:r,...t},s)=>a.jsx("input",{type:r,className:i("flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",o),ref:s,...t}));f.displayName="Input";export{f as I,p as S,b as X};
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+mv .env.example .env
+composer install --optimize-autoloader --no-dev
+
+
+php artisan key:generate
+php artisan migrate --seed --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
